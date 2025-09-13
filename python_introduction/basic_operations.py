f2ae804950bb75ac80ebe7063760c4db79250e8d
Nii Ayikwei Hammond <hammondayikwei@gmail.com>
2025-09-13 09:20:10 +0000

    basic Arithmetric operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2 
+substraction = number2 - number1
+multiplication = number1 * number2
+
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Substraction of {number1} and {number2} is {substraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
+
